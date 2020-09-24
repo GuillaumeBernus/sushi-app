a0a7f1af0dcf054b7d5bc5204e66dc1382a3d0b7
guillaume.bernus <guillaume.bernus@cdbdx.biz>
2020-09-24 17:40:59 +0200

    feat: add Header MUI

--- src/App/index.js
+++ src/App/index.js
@@ -1,7 +1,10 @@
 import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
 //import Greetings from "../Greetings";
 //import Counter from "../Counter";
 import Header from "../Header";
+import theme from "./theme";
+import { ThemeProvider } from "@material-ui/core/styles";
 export default function App() {
   return (
     /*<article>
@@ -10,9 +13,11 @@ export default function App() {
       <Counter beginAt={4} />
       <Counter />
     </article>*/
-    <div>
+
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Header title="Homepage" />
       <article>MAIN</article>
-    </div>
+    </ThemeProvider>
   );
 }
--- src/Header/index.js
+++ src/Header/index.js
@@ -1,22 +1,46 @@
 import React from "react";
 import { string } from "prop-types";
+import { ShoppingCart } from "@material-ui/icons";
+
+import {
+  AppBar,
+  IconButton,
+  Badge,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+
+import Avatar from "@material-ui/core/Avatar";
 
 import sushiLogo from "./logo_sushi.png";
-import { Logo } from "./styles";
-import { Title } from "./styles";
-import { Wrapper } from "./styles";
-import { Badge } from "./styles";
+import useStyles from "./useStyles";
+
+//styled-components
+// import { Logo } from "./styles";
+// import { Title } from "./styles";
+// import { Wrapper } from "./styles";
+// import { Badge } from "./styles";
+
 const webSiteTitle = "Les Sushi de Guigui";
 
 export default function Header({ title }) {
+  const classes = useStyles();
   return (
-    <Wrapper>
-      <Logo src={sushiLogo} alt={title} />
-      <Title>
-        {webSiteTitle} - {title}
-      </Title>
-      <Badge>3</Badge>
-    </Wrapper>
+    <div className={classes.root}>
+      <AppBar>
+        <Toolbar>
+          <Avatar alt={webSiteTitle} src={sushiLogo} className={classes.logo} />
+          <Typography variant="h6" className={classes.title}>
+            {webSiteTitle} - {title}
+          </Typography>
+          <IconButton aria-label="show 17 new notifications" color="inherit">
+            <Badge badgeContent={2} color="secondary">
+              <ShoppingCart />
+            </Badge>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 }
 
