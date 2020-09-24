4259fbdd642fb79fae5246540ab05ac53f6c4fe7
guillaume.bernus <guillaume.bernus@cdbdx.biz>
2020-09-24 15:13:54 +0200

    prepa App

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,15 @@
 import React from "react";
-import Greetings from "../Greetings";
-import Counter from "../Counter";
+//import Greetings from "../Greetings";
+//import Counter from "../Counter";
 
 export default function App() {
   return (
-    <article>
+    /*<article>
       <Greetings firstname="Guillaume" lastname="Bernus" />
       <Greetings firstname="Loulou" />
       <Counter beginAt={4} />
       <Counter />
-    </article>
+    </article>*/
+    <div></div>
   );
 }
