dda7c9a71aaed83eee1f1c16991a83393e231e37
guillaume.bernus <guillaume.bernus@cdbdx.biz>
2020-09-29 11:10:49 +0200

    feat: upgrade susicard

--- src/SushiCard/index.js
+++ src/SushiCard/index.js
@@ -6,32 +6,53 @@ import {
   CardActions,
   CardMedia,
   Typography,
+  Input,
 } from "@material-ui/core";
 
 import useStyles from "./styles";
+import { string } from "prop-types";
 
-export default function SushiCard() {
+import { AddBox, IndeterminateCheckBox } from "@material-ui/icons";
+import IconButton from "@material-ui/core/IconButton";
+
+export default function SushiCard({ image, title, description }) {
   const classes = useStyles();
   return (
     <Card className={classes.root}>
-      <CardMedia
-        className={classes.media}
-        image="https://images.sweetauthoring.com/recipe/43926_2090.jpg"
-        title="Paella dish"
-      />
+      <CardMedia className={classes.media} image={image} title={title} />
       <CardContent>
         <Typography gutterBottom variant="h5" component="h2">
-          Lizard Sashimi.
+          {title}
         </Typography>
         <Typography variant="body2" color="textSecondary" component="p">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the
-          mussels, if you like.
+          {description}
         </Typography>
+        <IconButton aria-label="delete" size="medium">
+          <IndeterminateCheckBox fontSize="inherit" />
+        </IconButton>
+        <Input
+          name="quantity"
+          value={1}
+          inputProps={{ "aria-label": "description" }}
+        />
+        <IconButton aria-label="add" size="medium">
+          <AddBox fontSize="inherit" />
+        </IconButton>
       </CardContent>
       <CardActions disableSpacing>
-        <Button size="small">Learn more</Button>
+        <Button size="small">Ajouter</Button>
       </CardActions>
     </Card>
   );
 }
+
+SushiCard.propTypes = {
+  image: string,
+  title: string,
+  description: string,
+};
+SushiCard.defaultProps = {
+  image: "https://images.sweetauthoring.com/recipe/43926_2090.jpg",
+  title: "Maki",
+  description: "Rice, fish, avocado,...",
+};
