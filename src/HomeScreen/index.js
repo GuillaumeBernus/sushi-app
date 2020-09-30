40d9b8bfe0453f30b584329ee8188700c4018eac
guillaume.bernus <guillaume.bernus@cdbdx.biz>
2020-09-30 15:26:39 +0200

    feat: search - add minimum length on search

--- src/HomeScreen/index.js
+++ src/HomeScreen/index.js
@@ -2,14 +2,21 @@ import React from "react";
 import Screen from "../Screen";
 import { TextField } from "@material-ui/core";
 import SushiCardList from "../SushiCardList";
+
 export default function HomeScreen() {
   const [search, setSearch] = React.useState("");
+
   const handleChange = (event) => {
     setSearch(event.target.value);
   };
   return (
     <Screen title="Homepage">
-      <TextField variant="outlined" value={search} onChange={handleChange} />
+      <TextField
+        variant="outlined"
+        value={search}
+        onChange={handleChange}
+        fullWidth
+      />
       <SushiCardList search={search} />
     </Screen>
   );
--- src/useSushis.js
+++ src/useSushis.js
@@ -1,9 +1,17 @@
 import { useQuery } from "react-query";
 import { isEmpty } from "lodash";
 import fetchYamato from "./fetchYamato";
+import config from "react-global-configuration";
 
+const { minLength } = config.get("search");
+const hasMinLength = (search) => search.length >= minLength;
 export default function useSushis(search) {
   let route = ["/sushis"];
-  if (!isEmpty(search)) route.push(`?q=${search}`);
-  return useQuery([route, search], () => fetchYamato(route.join("")));
+
+  if (!isEmpty(search) && hasMinLength(search)) {
+    route.push(`?q=${search}`);
+  }
+  return useQuery([route, hasMinLength(search) && search], () =>
+    fetchYamato(route.join(""))
+  );
 }
